b0d2f712e65e4556cd09eac692179b1e1b6d7be3
Phil B <phil@berryman.org.uk>
2018-09-25 12:16:35 +0100

    added burgers - not tested due to node issues

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import Menu from './Menu';
+
+const menu = [
+  {id:1,
+  burgerName : "Hamburger",
+  price : 10},
+  {id:2,
+    burgerName : "Cheese Burger",
+    price : 12},
+  {id:3,
+      burgerName : "Cat Burger",
+      price : 5},
+  {id:4,
+        burgerName : "Mystery Meat Burger",
+        price : "Free if you bring a cat"},
+]
 
 class App extends React.Component {
   render(){
     return (
       <div>
-        App will go here
+        <ul>
+          <Menu menu={menu} />
+        </ul>
+
+        <ul>
+
+        </ul>
       </div>
     )
   }
