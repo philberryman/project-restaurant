04cdf92510f6f9ed821c45d60af39429267193c1
Phil B <phil@berryman.org.uk>
2018-09-25 14:55:55 +0100

    added descriptions and images to burger

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,42 @@
 import React from "react";
 import Menu from "./Menu";
 import Locations from "./Locations";
-import Map from "./Map";
 
 const menu = [
   {
     id: 1,
     itemName: "Hamburger",
+    description: "Our classic burger. <br />Pickles, lettuce, tomator.<br />No cheese. Or cat. Probably",
+    imageURL:"hamburger.jpg",
     price: 10
   },
   {
     id: 2,
     itemName: "Cheese Burger",
+    description: "Our classic burger - with cheese.",
+    imageURL:"cheese-burgers.jpg",
     price: 12
   },
   {
     id: 3,
     itemName: "Cat Burger",
+    description: "Definitely no cat in this one. Cheese is optional",
+    imageURL:"cat-burger.jpg",
     price: 5
   },
   {
     id: 4,
     itemName: "Mystery Meat Burger",
-    price: "Free if you bring a cat"
+    description: "If you hate cats but love dogs this burger's for you. Or maybe if you hate dogs.",
+    imageURL:"mystery.jpg",
+    price: 20
+
   },
   {
     id: 5,
     itemName: "Fries",
+    description: "Fried potatoes.",
+    imageURL:"fries.jpg",
     price: "3"
   }
 ];
@@ -34,6 +44,7 @@ const locations = [
   {
     id: 1,
     location: "North London"
+    address: "113 Bayham St,<br />Camden Town, London NW1 0AG"
   },
   {
     id: 2,
@@ -64,9 +75,6 @@ class App extends React.Component {
             <ul className="locationsList">
               <Locations locations={locations} />
             </ul>
-          </div>
-          <div className="map">
-            <Map address={locations} />
           </div>
         </main>
       </div>
